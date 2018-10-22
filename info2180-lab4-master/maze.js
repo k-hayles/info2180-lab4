6c8e10a794b0acf7eedb80c90347e043c9bc44c9
k-hayles <44046662+k-hayles@users.noreply.github.com>
2018-10-22 01:43:55 -0500

    reset, status update and cheating functions added
    Added remaining functions

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -1,22 +1,59 @@
-var startValue = 0;
-var gameStatus = true;
+let bound;
+let touchWall =false;
+let endMessage;
+let starter;
+let checker = false;
+let oob;
+window.onload = function(){
+	gameStatus = document.getElementById('status');
+	endMessage= document.getElementById('end');
+	endMessage.onmouseover = win;
+	starter = document.getElementById('start');
+	oob = document.getElementById("maze");
+  starter.onclick = restart;
+	oob.onmouseleave = outOfBounds;
+	bound = document.querySelectorAll('.boundary');
+	for (var i = 0; i < bound.length - 1; i++){
+	bound[i].onmouseover = changeColor;
+	}
 
-window.onload = function () {
-    document.getElementById("boundary1").onmouseover = start;
-    
-    var bound = document.querySelectorAll(".boundary"); 
-    for (var i = 0; i <bound.length; i++)
-        
-        {
-            bound[i].onmouseover=function()
-            {
-                
-                for (var j=0; j<bound.length; j++)
-                    {
-                        bound[j].className= "boundary youlose";
-                    }
-            }
-        }
-    }
+ }
 
-   
+
+
+function changeColor(){
+	if(!checker){
+	if(!touchWall){
+	touchWall = true;
+	bound = document.querySelectorAll('.boundary');
+	gameStatus.innerHTML = "You lose";
+	for (var i = 0; i < bound.length - 1; i++){
+	bound[i].className += " youlose";
+ }
+ }
+	}
+}
+
+
+function win(){
+	if(!touchWall){
+	testwin =true;
+	gameStatus.innerHTML = "You win";
+	}
+}
+
+function restart(){
+	touchWall= false;
+	checker = false;
+	bound = document.querySelectorAll('.boundary');
+	gameStatus.innerHTML = "Move your mouse over S to begin";
+	for (var i = 0; i < bound.length - 1; i++){
+	bound[i].className = "boundary";
+	}
+}
+
+function outOfBounds(){
+	if(!checker){
+	changeColor();
+	}
+}
